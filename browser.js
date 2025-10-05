64e1188bfa30ec51f4ca3121c1f427924858cec1
Aykhan <ayxanshikhaliyevuv@gmail.com>
2025-10-05 12:45:20 +0400

    Update browser.js

--- browser.js
+++ browser.js
@@ -19,6 +19,7 @@ async function getBrowser() {
         '--disable-gpu',
         '--disable-dev-shm-usage'
       ],
+      executablePath: '/usr/bin/chromium',
       headless: true,
     });
   }
